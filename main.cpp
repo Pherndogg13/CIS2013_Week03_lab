93516dca478822bb9940607279dd4951e1c5fee9
BT228 <svc_OKC_BT228@ad.osuokc.edu>
2019-02-06 20:16:59 -0600

    Update main.cpp
    added the part division to the calculator program.

--- main.cpp
+++ main.cpp
@@ -1,42 +1,44 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int num1 = 0;
+int num2 = 0;
 	
+void printoptions(){
+	cout << "Enter your first number: ";
+	cin >> num1;
+	cout << "Enter your second number: ";
+	cin >> num2;
+}
+
+int main(){
 	char op = 'A';
 	char again = 'Y';
-	int num1 = 0;
-	int num2 = 0;
 	
 	cout << "Our C++ Calculator\n ";
 	
 	
-	while('n' != again) {
-		cout << "(A)dd or (S)ubtract?";
+	do {
+		cout << " (A)dd or (S)ubtract ";
 		cin >> op;
-		
-		cout << "Enter first number; ";
-		cin >> num1;
-		cout << "Enter second number; ";
-		cin >> num2;
-		
-	if(('A' == op )|| ('a' == op)) {
-		cout << num1 << " + " << num2 << " - " << (num1 + num2);
-	} else if (('S' == op) || ('s' == op)) {
-		cout << num1 << " - " << num2 << " = " << (num1 - num2);
-	} 
-	  else if (('S' == op) || ('s' == op)){
-		  cout << num1 << " x " << num2 << " = " << (num1 * num2);
+		switch ( op ) {
+		case 'a' || 'A':
+		printoptions();
+			cout << endl << num1 << " + " << num2 << " = " << (num1 + num2) << endl;
+		break;
+	case 's':
+	case 'S':
+		printoptions();
+		cout << endl << num1 << " - "
+			<< num2 << " = " << (num1 - num2) << endl;
+		break;
+	default:
+		cout << op << " is not a valid option..." << endl;
 	}
-	else {
-		cout << op << "is not a valid option....." << endl;
-	}
-		
-		
 		
 		cout << " Do you want to use Calculator again (Y/n)? ";
 		cin >> again;
-	}
+	} while ('n' != again);
 	
 	return 0;
 }
